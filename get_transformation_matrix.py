bf72cf61144ea1e0128fcf0415be6f6b7c21c3f8
Yanis Halloum <59097388+yanishalloum@users.noreply.github.com>
2025-04-26 01:02:58 -0700

    Update get_transformation_matrix.py

--- get_transformation_matrix.py
+++ get_transformation_matrix.py
@@ -3,38 +3,38 @@ def augmented_coord(p):
     return np.concatenate([x, np.ones((x.shape[0], 1))], axis=1)
  
 """Get transformation matrix defining the relative drone motion (rotation and translation) w.r.t previous frame'
-from a  given Fundamental matrix $F$ using Singular Value Decomposition (SVD)."""
-def get_transformation_matrix(F):
+from a  given Fundamental matrix F, and the intrinsic matrix K, using Singular Value Decomposition (SVD)."""
+def get_transformation_matrix(F, K):
+    # Compute essential matrix from fundamental matrix and intrinsics
+    E = K.T @ F @ K
+    
     # Define Matrix W to find F
     W = np.mat([[0, -1, 0], [1, 0, 0], [0, 0, 1]])
      
     # SVD to find the Fundamental matrix F
-    U, d, V = np.linalg.svd(F)
+    U, _, V = np.linalg.svd(E)
     assert np.linalg.det(U) > 0
  
-    # Make sure V's det is positive for it to be a rotation matrix
-    if np.linalg.det(V) < 0:
+    # Make sure U and V's det is positive for it to be a rotation matrix
+    if np.linalg.det(U) < 0:
+        U *= -1
+    if np.linalg.det(Vt) < 0:
         Vt *= -1
  
-    # R = (U.W).V
-    R = np.dot(np.dot(U, W), V)
- 
-    # If R's diagonal sum is positive, then it's a proper rotation matrix
-    # If not, R is defined as (U.(W.T).V
-    if np.sum(R.diagonal()) < 0:
-        R = np.dot(np.dot(U, W.T), V)
+    # Compute the rotation matrix
+    R = U @ W @ Vt
+    if np.trace(R) < 0:
+        R = U @ W.T @ Vt
  
     # Get translation vector t from U (3rd column)
     t = U[:, 2]
  
     # Define the T transformation matrix
-    T_matrix = np.eye(4)
+    T = np.eye(4)
  
     # T = [R|t]
-    T_matrix[:3, :3] = R
-    T_matrix[:3, 3] = t
- 
-    print(d)
+    T[:3, :3] = R
+    T[:3, 3] = t
  
     # Return the 4x4 homogeneous transformation matrix representing the pose
-    return T_matrix
+    return T
