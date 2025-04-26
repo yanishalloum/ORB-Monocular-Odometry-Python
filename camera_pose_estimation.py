95aac7598b6ffdf68fc81a0cb8441ca5b11a82ba
Yanis Halloum <59097388+yanishalloum@users.noreply.github.com>
2025-04-25 23:10:57 -0700

    Update camera_pose_estimation.py

--- camera_pose_estimation.py
+++ camera_pose_estimation.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 import os
 import matplotlib.pyplot as plt
-from test_matching import ORB_feature_detector, generate_orb_like_descriptors
+from feature_matching import ORB_feature_detector, generate_orb_like_descriptors
 
 # === Folder path ===
 img_dir = "./Datasets/EuRoc/MH01/mav0/cam0/data"
