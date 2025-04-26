5060ce79f290bdfb15f974db2d3f27ef816dda55
Yanis Halloum <59097388+yanishalloum@users.noreply.github.com>
2025-04-25 22:35:44 -0700

    Update and rename extract_feature.py to feature_extraction.py

--- feature_extraction.py
+++ feature_extraction.py
@@ -6,7 +6,7 @@ def ORB_feature_detector(image, N=12, T=20):
     h, w = image.shape
     keypoints = []
 
-    # Coords of the 16 pixels on the circle of radius 3 around p 
+    # Coordonnées des 16 pixels sur le cercle de rayon 3 autour de p
     circle_offsets = [
         (0, -3), (1, -3), (2, -2), (3, -1),
         (3, 0), (3, 1), (2, 2), (1, 3),
@@ -19,11 +19,11 @@ def ORB_feature_detector(image, N=12, T=20):
             I_p = int(image[y, x])
             circle_values = [int(image[y + dy, x + dx]) for dx, dy in circle_offsets]
 
-            # Define and high and low threshold
+            # Définir seuil haut et bas
             brighter = [val > I_p + T for val in circle_values]
             darker   = [val < I_p - T for val in circle_values]
 
-            # Verify if array has N consecutives points (avec wrap-around)
+            # Fonction utilitaire pour vérifier N consécutifs (avec wrap-around)
             def has_n_consecutive(arr, N):
                 extended = arr + arr[:N-1]  # wrap around
                 count = 0
@@ -41,18 +41,19 @@ def ORB_feature_detector(image, N=12, T=20):
 
     return keypoints
 
-# Test with an image
-image_path = "./Datasets/EuRoc/MH01/mav0/cam0/data/1403636579763555584.png"
-img = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+if __name__ == "__main__":
+    # Test avec une image
+    image_path = "./Datasets/EuRoc/MH01/mav0/cam0/data/1403636579763555584.png"
+    img = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
 
-# Get features
-features = ORB_feature_detector(img, N=11, T=30)
+    # Détection manuelle
+    features = ORB_feature_detector(img, N=11, T=30)
 
-# Display
-img_vis = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
-for x, y in features: 
-    cv2.circle(img_vis, (x, y), 2, (0, 255, 0), -1)
+    # Affichage
+    img_vis = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
+    for x, y in features: 
+        cv2.circle(img_vis, (x, y), 2, (0, 255, 0), -1)
 
-cv2.imshow("ORB Features", img_vis)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    cv2.imshow("ORB Features", img_vis)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
